c1d0d6e6cfb9d6b1c9225bf716699e019b007afc
genjdayo <69386279+genjdayo@users.noreply.github.com>
2021-02-08 13:02:51 +1030

    Initial commit

---
+++ script/index.js
@@ -0,0 +1,12 @@
+// close/open nav bar
+
+// $("#menu").click(function(){
+//         if ($(".navTab").css("display") == "none") {
+//                 $(".navTab").css("display", "block");
+//                 }
+//         else    $(".navTab").css("display", "none");
+//         });
+
+$("#menu").on("click", function(){
+        $(".mobi").toggle();
+      });
